1245039facd3dce0e207ab813680ab599232fddf
userschebo <164001924+userschebo@users.noreply.github.com>
2024-05-06 16:23:35 +0200

    Woche 1 Uebungsaufgaben hochgeladen

---
+++ RStudio/Uebungen/Woche 1/R12_Hilfe_2.R
@@ -0,0 +1,30 @@
+codeoceanR::rt_score()
+
+# A6 ----
+# Wieviele Seiten hat das RefCard von Tom Short?
+refCardAnzahlSeiten <- 4
+
+
+# A7 ----
+# Im Rstudio base R cheat sheet steht die Syntax, um die Doku von Funktionen aufzurufen.
+# (Gleich im Abschnitt Getting Help)
+# Welche Funktion wird dort als erstes Beispiel verwendet?
+cheatSheetFunktion <- "mean"
+
+
+# Pro Tipp: drucke eine RefCard deiner Wahl aus und markiere 
+# nach und nach farblich, was du alles gelernt und verstanden hast.
+
+
+# A8 ----
+# Was ist der Link zu aktuellen R Fragen auf Stackoverflow? (der https Teil ist optional)
+soLinkR <- "stackoverflow.com/questions/tagged/r"
+
+
+# A9 ----
+# Welchen Beitrag im Kursforum findest du bisher am hilfreichsten?
+forumLink <- "open.hpi.de/courses/programmieren-r2022/question/502427a0-1057-4568-9bef-1e619a6f6ee4"
+
+
+# Wenn du fertig bist, übermittle bitte deinen Punktestand an openHPI mit:
+# codeoceanR::rt_submit()
